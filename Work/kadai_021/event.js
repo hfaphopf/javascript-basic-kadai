3861637255cc2b644604068a01fc45b77c843096
hfaphopf <kuma.shou0925@gmail.com>
2023-10-30 07:41:16 +0900

    kadai_021をプッシュ

---
+++ Work/kadai_021/event.js
@@ -0,0 +1,12 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+// textというidを持つHTML要素を取得し、定数に代入する
+const push = document.getElementById('text');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+
+// ボタンが押下されるとテキストを変更する
+push.textContent = 'ボタンをクリックしました'
+});q
